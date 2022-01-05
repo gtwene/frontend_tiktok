6579c851e292d100b109becda726f63edda95ca0
Kwame <gtwene001@st.ug.edu.gh>
2022-01-05 02:44:00 +0000

    last

--- src/App.js
+++ src/App.js
@@ -5,8 +5,24 @@ function App() {
   return (
     <div className="app">
       <div className="app_videos">
-        <Video />
-        <Video />
+        <Video
+          url="https://d2of6bhnpl91ni.cloudfront.net/cms/1920x1080_Final_2020_Homepage%20Video-36f138a5ab.mp4"
+          channel="kwame"
+          description="YOOO I'm there missing you"
+          song="99 problems but REACT aint one!"
+          likes={123}
+          messages={456}
+          shares={789}
+        />
+        <Video
+          url="https://ak.picdn.net/shutterstock/videos/1034159981/preview/stock-footage-digital-information-flows-through-network-and-data-servers-behind-mesh-panels-in-a-server-room-of-a.webm"
+          channel="kwame"
+          description="YOOO I'm there missing you"
+          song="99 problems but REACT aint one!"
+          likes={999}
+          messages={456}
+          shares={789}
+        />
       </div>
 
       {/* app container */}
